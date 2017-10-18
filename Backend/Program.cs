91724c1d21e37958509e6e2e59035ee5c2a6de3b
SakalAndrej <andi.sakal15@gmail.com>
2017-10-18 09:33:14 +0200

    testing things

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -19,7 +19,7 @@ namespace Backend
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://192.88.24.155:80")
+            .UseUrls("http://absleo.htl-leonding.ac.at:8181")
             .UseStartup<Startup>()
             .Build();
     }
