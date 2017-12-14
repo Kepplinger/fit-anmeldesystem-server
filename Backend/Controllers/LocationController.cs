258e97847ca5aea27da8b407eed4acb84252c3b0
SakalAndrej <andi.sakal15@gmail.com>
2017-12-14 23:03:44 +0100

    fixed some bugs to the controllers

--- Backend/Controllers/LocationController.cs
+++ Backend/Controllers/LocationController.cs
@@ -11,8 +11,8 @@ using System.Threading.Tasks;
 namespace Backend.Controllers
 {
     [Route("api/[controller]")]
-
-    public class LocationController
+    [Produces("application/json", "application/xml")]
+    public class LocationController : Controller
     {
         private IUnitOfWork _unitOfWork;
 
@@ -21,46 +21,16 @@ namespace Backend.Controllers
             this._unitOfWork = uow;
         }
 
-        /// <summary>
-        /// Creates a Location Object.
-        /// </summary>
-        /// <response code="200">Returns the newly-created item</response>
-        /// <response code="101">If the item is null</response>
-        [HttpPut("Create")]
-        [ProducesResponseType(typeof(Location), 200)]
-        [ProducesResponseType(typeof(void), 101)]
-        public IActionResult Create([FromBody] Location temp)
-        {
-            System.Console.WriteLine(temp.Number);
-            try
-            {
-                if (temp != null)
-                {
-
-                    _unitOfWork.LocationRepository.Insert(temp);
-                    _unitOfWork.Save();
-                    //System.Console.WriteLine(temp.Company.Name);
-
-                    return new StatusCodeResult(StatusCodes.Status200OK);
-                }
-            }
-            catch (DbUpdateException ex)
-            {
-                System.Console.WriteLine(ex.Message);
-            }
-            return new StatusCodeResult(StatusCodes.Status101SwitchingProtocols);
-        }
-
         /// <response code="200">Returns all available Locations</response>
         /// <summary>
         /// Getting all Locations from Database
         /// </summary>
-        [HttpGet("GetAll")]
-        [ProducesResponseType(typeof(Location), 200)]
+        [HttpGet]
+        [ProducesResponseType(typeof(Location), StatusCodes.Status200OK)]
         public IActionResult GetAll()
         {
             var locations = _unitOfWork.LocationRepository.Get();
-            return new ObjectResult(locations);
+            return new OkObjectResult(locations);
         }
     }
 }
