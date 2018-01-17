afcfe0f3430f0f43ce35e0c671c821a378cb8b20
Unknown <felix.hofmann@liwest.at>
2018-01-17 11:28:07 +0100

    Base for controller

---
+++ Backend/Controllers/MediaController.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json", "application/xml")]
+    public class MediaController
+    {
+
+        private IUnitOfWork _unitOfWork;
+
+        public MediaController(IUnitOfWork uow)
+        {
+            this._unitOfWork = uow;
+        }
+    }
+}
