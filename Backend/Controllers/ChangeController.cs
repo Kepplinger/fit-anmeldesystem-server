13fbd7e886314eb5cc299d06c66298cfacb25d3e
SakalAndrej <andi.sakal15@gmail.com>
2018-03-21 23:44:07 +0100

    apply changes added

--- Backend/Controllers/ChangeController.cs
+++ Backend/Controllers/ChangeController.cs
@@ -36,64 +36,83 @@ namespace Backend.Controllers
             }
         }
 
+        [HttpPut("apply")]
+        [ProducesResponseType(typeof(ChangeProtocol), StatusCodes.Status200OK)]
+        public IActionResult applyChange([FromBody] int id)
+        {
+            if (id != null)
+            {
+                ChangeProtocol c = _unitOfWork.ChangeRepository.Get(p => p.Id == id).FirstOrDefault();
+                if (c != null)
+                {
+                    c.IsPending = false;
+                    return new OkObjectResult(c);
+                }
+            }
+            return new BadRequestResult();
+        }
+
 
         [HttpPut("revert")]
         [ProducesResponseType(typeof(ChangeProtocol), StatusCodes.Status200OK)]
         public IActionResult revertChange([FromBody] int id)
         {
-            Console.WriteLine(id);
-            ChangeProtocol change = _unitOfWork.ChangeRepository.Get(filter: c => c.Id == id).FirstOrDefault();
-            
-            switch (change.TableName)
+            if (id != null)
             {
-                case "Booking":
-                    Booking booking = _unitOfWork.BookingRepository.Get(filter: c => c.Id == id).FirstOrDefault();
-                    var bookingInfo = booking.GetType().GetProperty(change.ColumnName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-                    bookingInfo.SetValue(booking, change.OldValue, null);
-                    break;
-                case "Presentation":
-                    break;
-                case "Representative":
-                    break;
-                case "FolderInfo":
-                    break;
-                case "Contact":
-                    break;
-                case "Address":
-                    var comp = _unitOfWork.CompanyRepository.Get(p => p.Id == change.CompanyId, includeProperties: "Address").FirstOrDefault();
-                    Address address = _unitOfWork.AddressRepository.Get(filter: c => c.Id == comp.Address.Id).FirstOrDefault();
+                ChangeProtocol change = _unitOfWork.ChangeRepository.Get(filter: c => c.Id == id).FirstOrDefault();
+                if (change != null)
+                {
+                    switch (change.TableName)
+                    {
+                        case "Booking":
+                            Booking booking = _unitOfWork.BookingRepository.Get(filter: c => c.Id == id).FirstOrDefault();
+                            var bookingInfo = booking.GetType().GetProperty(change.ColumnName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+                            bookingInfo.SetValue(booking, change.OldValue, null);
+                            break;
+                        case "Presentation":
+                            break;
+                        case "Representative":
+                            break;
+                        case "FolderInfo":
+                            break;
+                        case "Contact":
+                            break;
+                        case "Address":
+                            var comp = _unitOfWork.CompanyRepository.Get(p => p.Id == change.CompanyId, includeProperties: "Address").FirstOrDefault();
+                            Address address = _unitOfWork.AddressRepository.Get(filter: c => c.Id == comp.Address.Id).FirstOrDefault();
 
-                    var addressInfo = address.GetType().GetProperty(change.ColumnName);
-                    addressInfo.SetValue(address, change.OldValue);
-                    _unitOfWork.AddressRepository.Update(address);
+                            var addressInfo = address.GetType().GetProperty(change.ColumnName);
+                            addressInfo.SetValue(address, change.OldValue);
+                            _unitOfWork.AddressRepository.Update(address);
 
-                    change.IsPending = false;
+                            change.IsPending = false;
 
-                    _unitOfWork.ChangeRepository.Update(change);
-                    _unitOfWork.Save();
+                            _unitOfWork.ChangeRepository.Update(change);
+                            _unitOfWork.Save();
 
-                    return new OkObjectResult(change);
-                    break;
-                case "Company":
-                    Company company = _unitOfWork.CompanyRepository.Get(p => p.Id == change.CompanyId, includeProperties: "Address").FirstOrDefault();
+                            return new OkObjectResult(change);
+                            break;
+                        case "Company":
+                            Company company = _unitOfWork.CompanyRepository.Get(p => p.Id == change.CompanyId, includeProperties: "Address").FirstOrDefault();
 
-                    var companyProperty = company.GetType().GetProperty(change.ColumnName);
-                    companyProperty.SetValue(company, change.OldValue);
-                    _unitOfWork.CompanyRepository.Update(company);
+                            var companyProperty = company.GetType().GetProperty(change.ColumnName);
+                            companyProperty.SetValue(company, change.OldValue);
+                            _unitOfWork.CompanyRepository.Update(company);
 
-                    change.IsPending = false;
+                            change.IsPending = false;
 
-                    _unitOfWork.ChangeRepository.Update(change);
-                    _unitOfWork.Save();
+                            _unitOfWork.ChangeRepository.Update(change);
+                            _unitOfWork.Save();
 
-                    return new OkObjectResult(change);
-                    break;
-                default:
-                    return new BadRequestResult();
-              
+                            return new OkObjectResult(change);
+                            break;
+                        default:
+                            return new BadRequestResult();
+
+                    }
+                }
             }
             return new BadRequestResult();
         }
-
     }
 }
