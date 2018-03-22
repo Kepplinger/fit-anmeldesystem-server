8fa7ba23a601cdd6168619e2b8fc94981ccaf62d
SakalAndrej <andi.sakal15@gmail.com>
2018-03-22 03:02:07 +0100

    media download fin

--- Backend/Controllers/MediaController.cs
+++ Backend/Controllers/MediaController.cs
@@ -17,6 +17,8 @@ namespace Backend.Controllers
     public class MediaController
     {
         private IUnitOfWork _unitOfWork;
+        string images = @"wwwroot/images/";
+        string temp = @"wwwroot/tempImages/";
 
         public MediaController(IUnitOfWork uow)
         {
@@ -32,10 +34,19 @@ namespace Backend.Controllers
                 .AddJsonFile("appsettings.json");
             var configuration = builder.Build();
             string url = configuration["Urls:DefaultUrl"];
-            Directory.CreateDirectory("wwwroot/tempImages/");
-            File.Copy("wwwroot/images/", "wwwroot/tempImages/");
+
+            string[] files = System.IO.Directory.GetFiles(images);
+
+            // Copy the files and overwrite destination files if they already exist.
+            foreach (string s in files)
+            {
+                // Use static Path methods to extract only the file name from the path.
+                string fileName = System.IO.Path.GetFileName(s);
+                string destFile = System.IO.Path.Combine(temp, fileName);
+                System.IO.File.Copy(s, destFile, true);
+            }
             ZipFile.CreateFromDirectory("wwwroot/tempImages/", "wwwroot/images/images.zip");
-            return new OkObjectResult("wwwroot/images/images.zip");
+            return new OkObjectResult(url + "/images/images.zip");
         }
     }
 }
