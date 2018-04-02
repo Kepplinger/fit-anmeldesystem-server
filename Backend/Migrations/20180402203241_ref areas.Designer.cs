0c834e35d0b5f06eaa4040bb3d9f168d7c2ee0c8
SakalAndrej <andi.sakal15@gmail.com>
2018-04-02 22:58:08 +0200

    ref graphicurl

--- Backend/Migrations/20180402203241_ref areas.Designer.cs
+++ Backend/Migrations/20180402203241_ref areas.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180402203241_ref areas")]
+    partial class refareas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20180402203241_ref areas.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Backend.Migrations
+{
+    public partial class refareas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GraphicURL",
+                table: "Areas",
+                newName: "GraphicUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GraphicUrl",
+                table: "Areas",
+                newName: "GraphicURL");
+        }
+    }
+}
