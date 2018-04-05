690859282bef649505e805dfdfd97eae32ee3c43
SakalAndrej <andi.sakal15@gmail.com>
2018-04-06 01:53:37 +0200

    gn8

--- Backend/Core/Entities/Event.cs
+++ Backend/Core/Entities/Event.cs
@@ -17,7 +17,6 @@ namespace Backend.Core.Entities
         [Required]
         public bool IsLocked { get; set; }
 
-        [Required]
         public List<Area> Areas { get; set; }
 
         public bool IsCurrent { get; set; }
