b379712e28304825e0162aae0d869f6572aa18e8
SakalAndrej <andi.sakal15@gmail.com>
2018-04-05 13:27:04 +0200

    email controller added update company and gets

--- Backend/Controllers/EmailController.cs
+++ Backend/Controllers/EmailController.cs
@@ -4,14 +4,77 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Backend.Utils;
+using StoreService.Persistence;
+using Backend.Core.Contracts;
+using Backend.Core.Entities;
 
 namespace Backend.Controllers
 {
     public class EmailController : Controller
     {
-        public IActionResult GetAllEmailName()
+        [HttpGet]
+        public IActionResult GetAllEmail()
         {
-            return new OkObjectResult(EmailHelper.emailTemplates);
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                List<Email> emails = uow.EmailRepository.Get().ToList();
+                if (emails != null && emails.Count > 0)
+                {
+                    return new OkObjectResult(emails);
+                }
+                else
+                {
+                    return new NoContentResult();
+                }
+            }
+        }
+        [HttpGet("byName/{paramName}")]
+        public IActionResult GetEmailByName(string name)
+        {
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                Email email = uow.EmailRepository.Get(m => m.Name.ToLower().Equals(name.ToLower())).FirstOrDefault();
+                if (email != null)
+                {
+                    return new OkObjectResult(email);
+                }
+                else
+                {
+                    return new NoContentResult();
+                }
+            }
+        }
+
+        [HttpGet("byId/{paramName}")]
+        public IActionResult GetEmailById(long id)
+        {
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                Email email = uow.EmailRepository.Get(m => m.Id == id).FirstOrDefault();
+                if (email != null)
+                {
+                    return new OkObjectResult(email);
+                }
+                else
+                {
+                    return new NoContentResult();
+                }
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMail([FromBody] Email email)
+        {
+            using (IUnitOfWork uow = new UnitOfWork())
+            {
+                if (uow.EmailRepository.Get(m => m.Id == email.Id).FirstOrDefault() != null)
+                {
+                    uow.EmailRepository.Update(email);
+                    uow.Save();
+                    return new OkResult();
+                }
+                return new BadRequestResult();
+            }
         }
     }
 }
