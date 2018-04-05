82284f32f688e83a8ec959202fd64288b557a987
SakalAndrej <andi.sakal15@gmail.com>
2018-04-06 01:53:00 +0200

    erd fixes

--- Backend/Core/Entities/ResourceBooking.cs
+++ Backend/Core/Entities/ResourceBooking.cs
@@ -1,13 +1,17 @@
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace Backend.Core.Entities
 {
     public class ResourceBooking : EntityObject
     {
+        [JsonIgnore]
+        [IgnoreDataMember]
         [ForeignKey("FK_Booking")]
         public Booking Booking { get; set; }
-        public int FK_Booking { get; set; }
+        public int? FK_Booking { get; set; }
 
         [ForeignKey("FK_Resource")]
         public Resource Resource { get; set; }
